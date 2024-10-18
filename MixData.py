22240f0213cb86c45991edb466242ad92e2ba2e8
unknown <nnicknamename@gmail.com>
2024-10-18 21:12:20 +0200

    rote the readme and some general changes.

--- MixData.py
+++ MixData.py
@@ -1,103 +1,118 @@
 from torch.utils.data import Dataset
 import torch
-import random 
+import random
+
+
 class MixableDataset(Dataset):
     r"""
     mixable dataset takes a list of :class:`Dataset` and combines them into one dataset
     """
-    def __init__(self,datasets):
+
+    def __init__(self, datasets):
         super().__init__()
-        self.datasets=datasets 
+        self.datasets = datasets
         self.init_Classes()
         self.extract_targets()
         self.calculate_length()
-        self.idx_table=[[i for i,x in enumerate(self.targets==c) if x==True] for c in range(len(self.classes))]
+        self.idx_table = [
+            [i for i, x in enumerate(self.targets == c) if x is True]
+            for c in range(len(self.classes))
+        ]
 
     def extract_targets(self):
-        targets=[] 
+        targets = []
         for dataset in self.datasets:
-            targets.extend(map(lambda x: self.transform_label(x,dataset),dataset.targets))
-        self.targets=torch.Tensor(targets)
+            targets.extend(
+                map(lambda x: self.transform_label(x, dataset), dataset.targets)
+            )
+        self.targets = torch.Tensor(targets)
+
     def init_Classes(self):
-        self.classes=[]
+        self.classes = []
         for dataset in self.datasets:
-            self.classes.extend(dataset.classes) 
+            self.classes.extend(dataset.classes)
 
     def calculate_length(self):
-        length=0
+        length = 0
         for dataset in self.datasets:
-            length+=dataset.__len__()
-        self.length=length
+            length += dataset.__len__()
+        self.length = length
 
     def __len__(self):
         return self.length
 
-    def map_index(self,index):
-        len=0
+    def map_index(self, index):
+        len = 0
         for dataset in self.datasets:
-            len+=dataset.__len__()
-            if len >index:
-                return dataset, index-(len-dataset.__len__())
-    
-    def transform_label(self,label,dataset):
+            len += dataset.__len__()
+            if len > index:
+                return dataset, index - (len - dataset.__len__())
+
+    def transform_label(self, label, dataset):
         return self.classes.index(dataset.classes[label])
 
-    #TODO: implement the transformation (its not important for now).
-    def transform_datapoint(self,datapoint):
+    # TODO: implement the transformation (its not important for now).
+    def transform_datapoint(self, datapoint):
         return datapoint
 
     def __getitem__(self, index):
-        dataset,index=self.map_index(index)
-        datapoint,label=dataset.__getitem__(index)
-        return self.transform_datapoint(datapoint),self.transform_label(label,dataset)
+        dataset, index = self.map_index(index)
+        datapoint, label = dataset.__getitem__(index)
+        return self.transform_datapoint(datapoint), self.transform_label(label, dataset)
 
-    def get_subDatast(self,focusClass,size=None):
-        if size==None:
-            dataset_idx=self.idx_table[focusClass].copy()
+    def get_subDatast(self, focusClass, size=None):
+        if size is None:
+            dataset_idx = self.idx_table[focusClass].copy()
         else:
-            assert size<=len(self.idx_table[focusClass]) , 'the given size is larger than the focus class'
-            dataset_idx=random.sample(self.idx_table[focusClass],size)
-        #dataset_idx=self.idx_table[focusClass].copy()
-        
-        for i in [i for i in range(len(self.classes)) if i != focusClass] :
-            s=int(len(dataset_idx)/(len(self.classes)))
-            if s>len(self.idx_table[i]):
-                s=len(self.idx_table[i])
+            assert size <= len(
+                self.idx_table[focusClass]
+            ), "the given size is larger than the focus class"
+            dataset_idx = random.sample(self.idx_table[focusClass], size)
+        # dataset_idx=self.idx_table[focusClass].copy()
+
+        for i in [i for i in range(len(self.classes)) if i != focusClass]:
+            s = int(len(dataset_idx) / (len(self.classes)))
+            if s > len(self.idx_table[i]):
+                s = len(self.idx_table[i])
                 dataset_idx.extend(self.idx_table[i])
-            dataset_idx.extend(random.sample(self.idx_table[i],s))
-        return Subdataset(self,dataset_idx,focusClass)
+            dataset_idx.extend(random.sample(self.idx_table[i], s))
+        return Subdataset(self, dataset_idx, focusClass)
+
 
 class Subdataset(Dataset):
     """
-    a :class:`Subdataset` is a dataset scentered around a class from a given dataset, this class is called a focusClass, where half the data is 
+    a :class:`Subdataset` is a dataset scentered around a class from a given dataset, this class is called a focusClass, where half the data is
         in the focus class the rest is a mix of the rest of the classes.
     """
-    def __init__(self,dataset,idx_table,focusClass):
+
+    def __init__(self, dataset, idx_table, focusClass):
         super().__init__()
-        self.dataset=dataset
-        self.idx_table=idx_table
-        self.focusClass=focusClass
+        self.dataset = dataset
+        self.idx_table = idx_table
+        self.focusClass = focusClass
         self.calculate_targets()
-        
+
     def calculate_targets(self):
-        targets=[]
+        targets = []
         for i in self.idx_table:
             targets.append(self.dataset.targets[i])
-        self.targets=torch.Tensor(targets)
+        self.targets = torch.Tensor(targets)
 
     def __len__(self):
         return len(self.idx_table)
 
-    def transform_label(self,label):
-        if label==self.focusClass:
+    def transform_label(self, label):
+        if label == self.focusClass:
             return torch.Tensor([1])
         else:
             return torch.Tensor([0])
-    def get_original_label(self,index):
-        _,label=self.dataset[self.idx_table[index]]
+
+    def get_original_label(self, index):
+        _, label = self.dataset[self.idx_table[index]]
         return label
+
     def __getitem__(self, index):
-        a=self.idx_table[index]
-        datapoint,label=self.dataset[a]
-        return datapoint,self.transform_label(label)
-        
+        a = self.idx_table[index]
+        datapoint, label = self.dataset[a]
+        return datapoint, self.transform_label(label)
+
--- SerMod.py
+++ SerMod.py
@@ -1,214 +1,288 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
-import torch.nn.functional as F
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from NnGen.debug import Debug
 from dask import delayed
 from dask import compute
 
+
 class SerMod(nn.Module):
-  def __init__(self,layer_spec):
-    super(SerMod, self).__init__()
-    self.layer_spec=layer_spec
-    self.stack=list()
-    for i in range(len(layer_spec)-1) :
-      lin=nn.Linear(layer_spec[i],layer_spec[i+1])
-      self.stack.append(lin)
-      if (i<len(layer_spec)-2):
-        Relu=nn.ReLU()
-        self.stack.append(Relu)
-      elif (i==len(layer_spec)-2):
-        sig=nn.Sigmoid()
-        self.stack.append(sig)
-    self.stack=nn.ModuleList(self.stack)
-  
-  def forward(self, x):
-    res=x
-    for e in self.stack:
-      res=e(res)
-    return res
+    def __init__(self, layer_spec):
+        super(SerMod, self).__init__()
+        self.layer_spec = layer_spec
+        self.stack = list()
+        for i in range(len(layer_spec) - 1):
+            lin = nn.Linear(layer_spec[i], layer_spec[i + 1])
+            self.stack.append(lin)
+            if i < len(layer_spec) - 2:
+                Relu = nn.ReLU()
+                self.stack.append(Relu)
+            elif i == len(layer_spec) - 2:
+                sig = nn.Sigmoid()
+                self.stack.append(sig)
+        self.stack = nn.ModuleList(self.stack)
 
-  @property
-  def parameter_vector(self):
-    res=torch.tensor([])
-    for i in range(0,len(self.stack),2):
-      res=torch.cat((res,self.serialize_layer(self.stack[i])))
-    return res
+    def forward(self, x):
+        res = x
+        for e in self.stack:
+            res = e(res)
+        return res
 
-  @parameter_vector.setter
-  def parameter_vector(self,ser):
-    k=0
-    for i in range(len(self.layer_spec)-1):
-      length=self.layer_spec[i]*self.layer_spec[i+1]+self.layer_spec[i+1]
-      l,ser=ser.split([length,len(ser)-length])
-      l_weight,l_bias =l.split([self.layer_spec[i]*self.layer_spec[i+1],self.layer_spec[i+1]])
-      l_wMat= torch.reshape(l_weight,[self.layer_spec[i+1],self.layer_spec[i]])
-      self.stack[k].load_state_dict({'weight':l_wMat,'bias':l_bias}, strict=False)
-      k+=2
+    @property
+    def parameter_vector(self):
+        res = torch.tensor([])
+        for i in range(0, len(self.stack), 2):
+            res = torch.cat((res, self.serialize_layer(self.stack[i])))
+        return res
 
-  def serialize_layer(self,layer):
-    l=layer.state_dict()
-    #extract weights
-    w=l['weight'].clone().detach().flatten()
-    #extract Biasis
-    b=l['bias'].clone().detach().flatten()
-    res=torch.cat((w,b))
-    return res
-    
+    @parameter_vector.setter
+    def parameter_vector(self, ser):
+        k = 0
+        for i in range(len(self.layer_spec) - 1):
+            length = (
+                self.layer_spec[i] * self.layer_spec[i + 1] + self.layer_spec[i + 1]
+            )
+            layer, ser = ser.split([length, len(ser) - length])
+            l_weight, l_bias = layer.split(
+                [self.layer_spec[i] * self.layer_spec[i + 1], self.layer_spec[i + 1]]
+            )
+            l_wMat = torch.reshape(
+                l_weight, [self.layer_spec[i + 1], self.layer_spec[i]]
+            )
+            self.stack[k].load_state_dict(
+                {"weight": l_wMat, "bias": l_bias}, strict=False
+            )
+            k += 2
 
-
-class light_serial_model(pl.LightningModule):
-  def __init__(self,model_vector,model_spec,learning_rate,randomInint=False):
-    super().__init__()
-    self.model_spec=model_spec
-    self.model= SerMod(model_spec)
-    if not randomInint:
-      self.model.parameter_vector=model_vector
-    self.learning_rate=learning_rate
-
-  def forward(self,x):
-    return self.model(x)
-
-  def training_step(self,batch,batch_idx):
-    images,labels=batch 
-    images=images.reshape(-1,self.model_spec[0])
-    output=self.forward(images)  
-    Loss=F.mse_loss(output,labels)
-    self.log('train_loss',Loss)
-    return Loss
-
-  def test_step(self,batch,batch_idx):
-    images,labels=batch 
-    images=images.reshape(-1,self.model_spec[0])
-    output=self.forward(images)  
-    Loss=F.mse_loss(output,labels)
-    self.log("val_loss",Loss)   
-
-  def configure_optimizers(self):
-    return torch.optim.Adam(self.parameters(),lr=self.learning_rate)
-
-
-class Trainer:
-  def __init__(self,batch_size,lr_rate,nb_epoch,train_dataset,model_spec,test_dataset=None,train_dataset_size=None,random_init=False,num_workers=2,gpus=0):
-    self.batch_size=batch_size
-    self.lr_rate=lr_rate
-    self.nb_epochs=nb_epoch
-    self.train_dataset=train_dataset
-    self.model_spec=model_spec
-    self.gpus=gpus
-    self.random_init=random_init
-    self.num_workers=num_workers
-    self.test_dataset=test_dataset
-    self.train_dataset_size=train_dataset_size
-  def create_model(self,data):
-    model_vector,_=data
-    return light_serial_model(model_vector,self.model_spec,self.lr_rate,randomInint=self.random_init)
-  
-  def create_dataLoader(self,data):
-    _,clas=data
-    return DataLoader(dataset=self.train_dataset.get_subDatast(clas,self.train_dataset_size),batch_size=self.batch_size,shuffle=True,num_workers=self.num_workers,pin_memory=True)
-  
-  def create_test_dataloader(self,data):
-    _,clas=data
-    return DataLoader(dataset=self.test_dataset.get_subDatast(clas),batch_size=self.batch_size,num_workers=self.num_workers,pin_memory=True)
-  
-  def train(self,data):
-    model,subdataloader=data
-    model_trainer=pl.Trainer(callbacks=[EarlyStopping(monitor="train_loss",min_delta=0.0, mode="min")],max_epochs=self.nb_epochs,enable_checkpointing=False,enable_model_summary=False,enable_progress_bar=True,logger=False,gpus=self.gpus)
-    model_trainer.fit(model=model,train_dataloaders=subdataloader)
-
-  def test(self,data):
-    model,subdataloader=data
-    model_trainer=pl.Trainer(max_epochs=self.nb_epochs,enable_checkpointing=False,enable_model_summary=False,enable_progress_bar=True,logger=False,gpus=self.gpus)
-    return model_trainer.test(model=model,dataloaders=subdataloader)[0]['val_loss']
-
-  def parallel_exec(self,function,data):
-    list_of_delayed_functions=[delayed(function)(datapoint) for datapoint in data]
-    results=compute(list_of_delayed_functions, num_workers=self.num_workers)[0]
-    return results
-
-  def get_test_results(self):
-    assert self.pretest_results is not None or self.posttest_results is not None, 'you did not run any tests'
-    return {'pretest':self.pretest_results,'posttest':self.posttest_results}
-
-
-  def serialize_models(self,models):
-    res=None
-    for model in models:
-      serial_model=model.model.parameter_vector
-      if res==None:
-        res=serial_model.reshape(-1,len(serial_model))
-      else:
-        res=torch.cat([res,serial_model.reshape(-1,len(serial_model))])
-    return res
-
-  def train_models(self,data,pretest=False,posttest=False):
-    if (pretest or posttest):
-      assert self.test_dataset is not None , 'no dataset to test with'
-    models=[self.create_model(m) for m in data]
-    train_dataloaders=[self.create_dataLoader(m) for m in data]
-    test_dataoaders=[self.create_test_dataloader(m) for m in data]
-    if pretest:
-      self.pretest_results=self.parallel_exec(self.test,zip(models,test_dataoaders))
-
-    self.parallel_exec(self.train,zip(models,train_dataloaders))
-    if posttest:
-      self.posttest_results=self.parallel_exec(self.test,zip(models,test_dataoaders))
-    return self.serialize_models(models) 
-
-
-class Trainer2 :
-    def __init__(self,batch_size,lr_rate,nb_epoch,input_size,dataset,num_workers=2,zeroTrain=False):
-        self.batch_size=batch_size
-        self.lr_rate=lr_rate
-        self.nb_epoch=nb_epoch
-        self.input_size=input_size
-        self.debug=Debug()
-        self.zeroTrain=zeroTrain
-        self.train_dataset=dataset
-        self.num_workers=num_workers
-
-    def train(self,data,modelSpec):
-        models=[]
-        for i,d in enumerate(data):
-            vmodel,clas=d
-            model=SerMod(modelSpec)
-            if not self.zeroTrain:
-              model.parameter_vector=vmodel
-            dataloader=DataLoader(self.train_dataset.get_subDatast(clas),num_workers=self.num_workers,batch_size=self.batch_size,shuffle=True,pin_memory=True)
-            models.append({'model':model,'loader':dataloader,'idx':i})
-      
-        list_of_delayed_functions = []
-        for d in models:
-          list_of_delayed_functions.append(delayed(self.train_model)(d))
-        models_loss=compute(list_of_delayed_functions)
-        
-        #models_loss=map(self.train_model,models)
-        #print(model.serialize_model())
-        res=None
-        for model in models:
-          serial_model=model['model'].parameter_vector
-          if res==None:
-            res=serial_model.reshape(-1,len(serial_model))
-          else:
-            res=torch.cat([res,serial_model.reshape(-1,len(serial_model))])
+    def serialize_layer(self, layer):
+        layer = layer.state_dict()
+        # extract weights
+        w = layer["weight"].clone().detach().flatten()
+        # extract Biasis
+        b = layer["bias"].clone().detach().flatten()
+        res = torch.cat((w, b))
         return res
 
 
-    def train_model(self ,data):
-      loss=nn.MSELoss()
-      optimizer=torch.optim.Adam(data['model'].parameters(),lr=self.lr_rate)
-      for epoch in range(self.nb_epoch):
-        for i,(images,labels) in enumerate(data['loader']):
-          images=images.reshape(-1,self.input_size)
-          output=data['model'](images)  
-          Loss=loss(output,labels)
-          optimizer.zero_grad()
-          Loss.backward()
-          optimizer.step()
-          self.debug.writeUpdate('  model:'+str(data['idx'])+'  epoch:'+str(epoch)+'  Loss:'+str(Loss.item()))
-      self.debug.write('\n')
-      return Loss.item()
+class light_serial_model(pl.LightningModule):
+    def __init__(self, model_vector, model_spec, learning_rate, randomInint=False):
+        super().__init__()
+        self.model_spec = model_spec
+        self.model = SerMod(model_spec)
+        if not randomInint:
+            self.model.parameter_vector = model_vector
+        self.learning_rate = learning_rate
 
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        images, labels = batch
+        images = images.reshape(-1, self.model_spec[0])
+        output = self.forward(images)
+        Loss = F.mse_loss(output, labels)
+        self.log("train_loss", Loss)
+        return Loss
+
+    def test_step(self, batch, batch_idx):
+        images, labels = batch
+        images = images.reshape(-1, self.model_spec[0])
+        output = self.forward(images)
+        Loss = F.mse_loss(output, labels)
+        self.log("val_loss", Loss)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+
+class Trainer:
+    def __init__(
+        self,
+        batch_size,
+        lr_rate,
+        nb_epoch,
+        train_dataset,
+        model_spec,
+        test_dataset=None,
+        train_dataset_size=None,
+        random_init=False,
+        num_workers=2,
+        gpus=0,
+    ):
+        self.batch_size = batch_size
+        self.lr_rate = lr_rate
+        self.nb_epochs = nb_epoch
+        self.train_dataset = train_dataset
+        self.model_spec = model_spec
+        self.gpus = gpus
+        self.random_init = random_init
+        self.num_workers = num_workers
+        self.test_dataset = test_dataset
+        self.train_dataset_size = train_dataset_size
+
+    def create_model(self, data):
+        model_vector, _ = data
+        return light_serial_model(
+            model_vector, self.model_spec, self.lr_rate, randomInint=self.random_init
+        )
+
+    def create_dataLoader(self, data):
+        _, clas = data
+        return DataLoader(
+            dataset=self.train_dataset.get_subDatast(clas, self.train_dataset_size),
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+    def create_test_dataloader(self, data):
+        _, clas = data
+        return DataLoader(
+            dataset=self.test_dataset.get_subDatast(clas),
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
+    def train(self, data):
+        model, subdataloader = data
+        model_trainer = pl.Trainer(
+            callbacks=[EarlyStopping(monitor="train_loss", min_delta=0.0, mode="min")],
+            max_epochs=self.nb_epochs,
+            enable_checkpointing=False,
+            enable_model_summary=False,
+            enable_progress_bar=True,
+            logger=False,
+            gpus=self.gpus,
+        )
+        model_trainer.fit(model=model, train_dataloaders=subdataloader)
+
+    def test(self, data):
+        model, subdataloader = data
+        model_trainer = pl.Trainer(
+            max_epochs=self.nb_epochs,
+            enable_checkpointing=False,
+            enable_model_summary=False,
+            enable_progress_bar=True,
+            logger=False,
+            gpus=self.gpus,
+        )
+        return model_trainer.test(model=model, dataloaders=subdataloader)[0]["val_loss"]
+
+    def parallel_exec(self, function, data):
+        list_of_delayed_functions = [delayed(function)(datapoint) for datapoint in data]
+        results = compute(list_of_delayed_functions, num_workers=self.num_workers)[0]
+        return results
+
+    def get_test_results(self):
+        assert (
+            self.pretest_results is not None or self.posttest_results is not None
+        ), "you did not run any tests"
+        return {"pretest": self.pretest_results, "posttest": self.posttest_results}
+
+    def serialize_models(self, models):
+        res = None
+        for model in models:
+            serial_model = model.model.parameter_vector
+            if res is None:
+                res = serial_model.reshape(-1, len(serial_model))
+            else:
+                res = torch.cat([res, serial_model.reshape(-1, len(serial_model))])
+        return res
+
+    def train_models(self, data, pretest=False, posttest=False):
+        if pretest or posttest:
+            assert self.test_dataset is not None, "no dataset to test with"
+        models = [self.create_model(m) for m in data]
+        train_dataloaders = [self.create_dataLoader(m) for m in data]
+        test_dataoaders = [self.create_test_dataloader(m) for m in data]
+        if pretest:
+            self.pretest_results = self.parallel_exec(
+                self.test, zip(models, test_dataoaders)
+            )
+
+        self.parallel_exec(self.train, zip(models, train_dataloaders))
+        if posttest:
+            self.posttest_results = self.parallel_exec(
+                self.test, zip(models, test_dataoaders)
+            )
+        return self.serialize_models(models)
+
+
+class Trainer2:
+    def __init__(
+        self,
+        batch_size,
+        lr_rate,
+        nb_epoch,
+        input_size,
+        dataset,
+        num_workers=2,
+        zeroTrain=False,
+    ):
+        self.batch_size = batch_size
+        self.lr_rate = lr_rate
+        self.nb_epoch = nb_epoch
+        self.input_size = input_size
+        self.debug = Debug()
+        self.zeroTrain = zeroTrain
+        self.train_dataset = dataset
+        self.num_workers = num_workers
+
+    def train(self, data, modelSpec):
+        models = []
+        for i, d in enumerate(data):
+            vmodel, clas = d
+            model = SerMod(modelSpec)
+            if not self.zeroTrain:
+                model.parameter_vector = vmodel
+            dataloader = DataLoader(
+                self.train_dataset.get_subDatast(clas),
+                num_workers=self.num_workers,
+                batch_size=self.batch_size,
+                shuffle=True,
+                pin_memory=True,
+            )
+            models.append({"model": model, "loader": dataloader, "idx": i})
+
+        list_of_delayed_functions = []
+        for d in models:
+            list_of_delayed_functions.append(delayed(self.train_model)(d))
+        # models_loss = compute(list_of_delayed_functions)
+
+        # models_loss=map(self.train_model,models)
+        # print(model.serialize_model())
+        res = None
+        for model in models:
+            serial_model = model["model"].parameter_vector
+            if res is None:
+                res = serial_model.reshape(-1, len(serial_model))
+            else:
+                res = torch.cat([res, serial_model.reshape(-1, len(serial_model))])
+        return res
+
+    def train_model(self, data):
+        loss = nn.MSELoss()
+        optimizer = torch.optim.Adam(data["model"].parameters(), lr=self.lr_rate)
+        for epoch in range(self.nb_epoch):
+            for i, (images, labels) in enumerate(data["loader"]):
+                images = images.reshape(-1, self.input_size)
+                output = data["model"](images)
+                Loss = loss(output, labels)
+                optimizer.zero_grad()
+                Loss.backward()
+                optimizer.step()
+                self.debug.writeUpdate(
+                    "  model:"
+                    + str(data["idx"])
+                    + "  epoch:"
+                    + str(epoch)
+                    + "  Loss:"
+                    + str(Loss.item())
+                )
+        self.debug.write("\n")
+        return Loss.item()
